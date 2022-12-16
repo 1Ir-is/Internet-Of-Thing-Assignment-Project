fe9b5e2467f7224a9180d89a3038058a82ce6c8e
IriS <mhuynk1005@gmail.com>
2022-12-16 18:31:13 +0700

    update code for gate

--- src/gate_code.cpp
+++ src/gate_code.cpp
@@ -4,8 +4,8 @@
  
 #define SS_PIN 10
 #define RST_PIN 9
-#define LED_G 4 //define green LED pin
-#define LED_R 5 //define red LED
+#define LED_R 4 //define red LED pin 
+#define LED_B 5 //define blue LED
 #define BUZZER 2 //buzzer pin
 MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
 Servo myServo; //define servo name
@@ -19,8 +19,8 @@ void setup()
   mfrc522.PCD_Init();   // Initiate MFRC522
   myServo.attach(3); //servo pin
   myServo.write(0); //servo start position
-  pinMode(LED_G, OUTPUT);
   pinMode(LED_R, OUTPUT);
+  pinMode(LED_B, OUTPUT);
   pinMode(BUZZER, OUTPUT);
   pinMode(ir, INPUT);
   noTone(BUZZER);
@@ -46,46 +46,44 @@ void loop()
   byte letter;
   for (byte i = 0; i < mfrc522.uid.size; i++) 
   {
-     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-     Serial.print(mfrc522.uid.uidByte[i], DEC);
-     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
-     content.concat(String(mfrc522.uid.uidByte[i], DEC));
+    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+    Serial.print(mfrc522.uid.uidByte[i], DEC);
+    content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+    content.concat(String(mfrc522.uid.uidByte[i], DEC));
   }
   Serial.println();
   Serial.print("Message : ");
   content.toUpperCase();
-  if (content.substring(1) == "91 120 87 00") //change here the UID of the card/cards that you want to give access
+  if (digitalRead(ir) == LOW)
   {
-    Serial.println("Authorized access");
-    delay(500);
-    digitalWrite(LED_G, HIGH);
-    tone(BUZZER, 500);
-    delay(300);
-    noTone(BUZZER);
-    myServo.write(90);
-    Serial.println("Gate Opened");
-    delay(2500);
-    if (digitalRead(ir) == LOW)
+    if (content.substring(1) == "91 120 87 00") //change here the UID of the card/cards that you want to give access
     {
-      myServo.write(0);
-      Serial.println("Gate Closed");
-      Serial.println();
-      digitalWrite(LED_G, LOW);
-    }
+      Serial.println("Authorized access");
+      delay(500);
+      digitalWrite(LED_R, HIGH);
+      tone(BUZZER, 500);
+      delay(300);
+      noTone(BUZZER);
+      myServo.write(90);
+      Serial.println("Gate Opened");
+      delay(2500);
+      if (digitalRead(ir) == HIGH)
+      {
+        myServo.write(0);
+        Serial.println("Gate Closed");
+        Serial.println();
+        digitalWrite(LED_R, LOW);
+      }
+    }  
     else
     {
-      return;
+      Serial.println(" Access denied");
+      Serial.println();
+      digitalWrite(LED_B, HIGH);
+      tone(BUZZER, 300);
+      delay(1000);
+      digitalWrite(LED_B, LOW);
+      noTone(BUZZER);
     }
-
-  }
- else 
-  {
-    Serial.println(" Access denied");
-    Serial.println();
-    digitalWrite(LED_R, HIGH);
-    tone(BUZZER, 300);
-    delay(1000);
-    digitalWrite(LED_R, LOW);
-    noTone(BUZZER);
-  }
+  } 
 }
